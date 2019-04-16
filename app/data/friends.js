80ab61175ca13aca8aafff5cc7cdbef81c08adc5
Tamar Auber <mischegoss@gmail.com>
2019-04-16 13:57:26 -0400

    Basic blank redone

---
+++ app/data/friends.js
@@ -0,0 +1,106 @@
+/* This is the data to populate the stuff  to start. The format is just copied and  pasted  from example
+  * Need to add photos */
+
+var friends = [
+    {
+      name: "Ahmed",
+      photo: "",
+      scores: [
+        "5",
+        "1",
+        "4",
+        "4",
+        "5",
+        "1",
+        "2",
+        "5",
+        "4",
+        "1"
+      ]
+    },
+    {
+      name: "Andrew",
+      photo: "",
+      scores: [
+        "4",
+        "2",
+        "5",
+        "1",
+        "3",
+        "2",
+        "2",
+        "1",
+        "3",
+        "2"
+      ]
+    },
+    {
+      name: "Tom",
+      photo: "",
+      scores: [
+        "5",
+        "2",
+        "2",
+        "4",
+        "4",
+        "1",
+        "4",
+        "2",
+        "5",
+        "5"
+      ]
+    },
+    {
+      name: "Cornelia",
+      photo: "",
+      scores: [
+        "3",
+        "3",
+        "4",
+        "2",
+        "2",
+        "1",
+        "3",
+        "2",
+        "2",
+        "3"
+      ]
+    },
+    {
+      name: "Joe",
+      photo: "",
+      scores: [
+        "4",
+        "3",
+        "5",
+        "1",
+        "5",
+        "2",
+        "5",
+        "3",
+        "1",
+        "4"
+      ]
+    },
+    {
+      name: "Sarah",
+      photo: "",
+      scores: [
+        "4",
+        "4",
+        "5",
+        "3",
+        "2",
+        "2",
+        "5",
+        "5",
+        "5",
+        "5"
+      ]
+    }
+  
+  ];
+  
+  // Here we export the array. This makes it accessible to other files using require.
+  module.exports = friends;
+  
---
+++ server.js
@@ -0,0 +1,20 @@
+
+/* This is just a standard express server as demonstrated in class.
+ * I had initially used bodyparser,  but it out to use this
+ * configuration based on  class demo */
+
+
+var express = require("express");
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+  });
+  
